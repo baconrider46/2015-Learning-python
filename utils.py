908064b1b436c170ac337f53ed012b6475d9e5ca
Aidan Marino <antiquekid20@gmail.com>
2015-06-10 20:41:56 -0400

    save

---
+++ utils.py
@@ -0,0 +1,17 @@
+"""THis is my utility sink"""
+
+import colors as c
+
+def ask(question,color=c.magenta):
+    print(color + question + c.reset)
+    answer = input("> " + c.base3).lower().strip()
+    print(c.reset)
+    return answer
+
+if __name__ == '__main__':
+    print(c.clear)
+    color = ask("what is your name in color" ,c.random_color())
+    name = ask("Whats your name young one")
+    
+
+
